8d1e5a8d3899d0f8cbbfd7594dda277cf1eefc0c
s19151 <s19151@pjwstk.edu.pl>
2020-03-02 17:04:57 +0100

    Dodalem projekt

---
+++ cwiczenia1/cwiczenia1/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace cwiczenia1 {
+    public class Program  {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
